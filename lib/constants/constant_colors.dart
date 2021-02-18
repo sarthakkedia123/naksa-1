9024d69d6e06135be3275b6b84c163a00d231d38
Bikash Bhuyan <bikashbhuyan802@gmail.com>
2021-02-18 12:22:24 +0530

    Add files via upload

---
+++ lib/constants/constant_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+
+final Color bodyBlackColor = Color(0xff393e46);
+final Color bodyWhiteColor = Color(0xffffffff);
+final Color buttonColor = Color(0xffec4646);
